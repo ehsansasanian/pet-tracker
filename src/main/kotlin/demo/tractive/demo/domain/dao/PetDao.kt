cbf7a927569505092cd8c0fd25f070cb6ed856b9
Ehsan <ehsan.sasanian@gmail.com>
2025-05-12 16:49:10 +0200

    add findById abstraction to PetDao

--- src/main/kotlin/demo/tractive/demo/domain/dao/PetDao.kt
+++ src/main/kotlin/demo/tractive/demo/domain/dao/PetDao.kt
@@ -5,6 +5,8 @@ import demo.tractive.demo.domain.model.PetType
 import demo.tractive.demo.domain.model.TrackerType
 
 interface PetDao {
+    fun findById(id: Long): PetEntity?
+
     fun findAll(inZone: Boolean?): List<PetEntity>
 
     fun save(petEntity: PetEntity): PetEntity
